6f0e8fc29601c2fdbe27ab9b427384f6049f162a
msplawn <mmsplawn@gmail.com>
2022-10-31 13:40:10 -0500

    added options to booking component, added parachute icon to parasaillanding component, failed to center icon

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import 'semantic-ui-css/semantic.min.css';
 import TopMenu from './components/Menu/Menu';
 import LandingInfo from './components/LandingInfo/LandingInfo';
 import BookingForm from './components/BookingForm/BookingForm';
+import ParasailLanding from './components/ParasailLanding/ParasailLanding';
+import JetskiLanding from './components/JetskiLanding/JetskiLanding';
 
 function App() {
   return (
     <div className="App">
       <TopMenu />
       <LandingInfo />
+      <ParasailLanding />
+      <JetskiLanding />
       <BookingForm />
     </div>
   );
