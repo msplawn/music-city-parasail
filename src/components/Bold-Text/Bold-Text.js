db1de751141c012afb2a93d1cb4587045c5befda
msplawn <mmsplawn@gmail.com>
2022-12-10 14:40:40 -0600

    added bold text section and unconnected about component

---
+++ src/components/Bold-Text/Bold-Text.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './bold-text.css';
+
+const BoldText = () => (
+    <div id='bold'>
+        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+
+    </div>
+)
+
+export default BoldText;
