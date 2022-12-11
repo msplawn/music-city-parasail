cc0840ac1b0dbbbc90224b07723ec885af5a283b
Count Zero <joelecox1@gmail.com>
2022-12-10 19:44:19 -0600

    da biggest bird

--- src/components/JetskiLanding/JetskiLanding.js
+++ src/components/JetskiLanding/JetskiLanding.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Button, Header, Icon, Segment } from 'semantic-ui-react'
 import './jetski-landing.css';
+import jetski from './assets/jetski.svg'
 
 
 const JetskiLanding = () => (
   <div id="jetski-landing" placeholder>
     <Header id="header" icon>
-      <Icon id='icon' name='pdf file outline' />
+      {/* <Icon id='icon' name='pdf file outline' /> */}
+      <object className='jetski-icon' type="image/svg+xml" data={jetski}>svg-animation</object>
       <div id='title'>
         Jetskiing Experiences
       </div>
