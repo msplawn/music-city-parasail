7bd716286cc47fd18f1cd725b85724a4cc024f27
Count Zero <joelecox1@gmail.com>
2022-12-14 18:52:30 -0600

    temp logo trial with updated hero and intro sections

--- src/components/LandingInfo/LandingInfo.js
+++ src/components/LandingInfo/LandingInfo.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Button, Header, Icon } from 'semantic-ui-react';
+import paraboat from './assets/paraswag2.svg'
 import './landing.css';
 
 
 const LandingInfo = () => (
   <div id="landing" className="segment" placeholder>
     <Header id="header" icon>
-      <Icon id="icon" name='pdf file outline' />
-      <div id="title">
+      {/* <Icon id="icon" name='pdf file outline' /> */}
+      <object className='paraboat' type="image/svg+xml" data={paraboat}>svg-animation</object>
+      {/* <div id="title">
         Music City Parasail
-      </div>
+      </div> */}
     </Header>
     {/* <Segment > */}
-    <div className='para'>
+    {/* <div className='para'>
       Music City Parasail is the premiere destination for parasailing and jetski experiences in Nashville! We offer 15 and 30 minute parasailing experiences as well as jetski rentals to enjoy on beautiful Percy Priest Lake.
-    </div>
+    </div> */}
     {/* </Segment> */}
-    <Button id="book-button" primary>Book an experience!</Button>
+    {/* <Button id="book-button" primary>Book an experience!</Button> */}
   </div>
 )
 
